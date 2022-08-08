9eecd38252bf768eecd46980facf2c486e08b65b
montu garg <montu0879.be20@chitkara.edu.in>
2022-08-08 18:57:18 +0530

    8 aug

--- 5 aug/5 aug.js
+++ 5 aug/5 aug.js
@@ -1,14 +1,15 @@
 function myfun(){
-    let a=document.getElementById("n1").value;
-    a=Number(a);
     let sum=0;
     let count=0;
-    while(a>0){
-        sum=sum+a%10;
-        a=a/10;
-        a=Math.trunc(a);
+    sum=Number(sum);
+    function change(value){
+        sum=sum+Number(value);
         count=count+1;
+        return Number(value);
     }
+    let a=document.getElementById("n1").value;
+    let arr=a.split("");
+    arr.map(change);
     if(count==5){
         document.getElementById("square").style.width=sum+"px";
         document.getElementById("square").style.height=sum+"px";
@@ -20,6 +21,7 @@ function myfun(){
         document.getElementById("p1").innerHTML="Please enter 5 digits.";
         document.getElementById("square").style.width="0px";
         document.getElementById("square").style.height="0px";
+        document.getElementById("square").style.transitionProperty="none";
 
     }
     
